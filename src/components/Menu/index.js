1ccc563f4e4dc16f346962e1f1df56dec72f0e44
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-26 14:17:07 -0300

    Criando media query

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,46 @@
 import {Link} from 'react-router-dom'
+import {useState, useEffect} from 'react'
 
 import './Menu.css'
 
 export default function Menu() {
+
+    const [itensVisible, setItensVisible] = useState(false)
+
+    useEffect(() => {
+        function mudouTamanho() {
+            setItensVisible(window.innerWidth >= 646)
+        }
+
+        window.addEventListener('resize', mudouTamanho);
+
+        mudouTamanho();
+
+        return () => {
+            window.removeEventListener('resize', mudouTamanho);
+          };
+    }, [])
+    
+    function clickMenu() {
+        setItensVisible(!itensVisible)
+    }
+
+    function hideMenu() {
+        if (window.innerWidth <= 646) {
+            setItensVisible(false)
+        }
+    }
+
     return (
-        <menu>
-            <Link to='/'>Home</Link>
-            <Link to='/historia'>História</Link>
-            <Link to='/titulos'>Títulos</Link>
-            <Link to='/estatisticas'>Estatísticas</Link>
-            <Link to='/videos'>Vídeos</Link>
-        </menu>
+        <div>
+            <span id="burguer" class="material-symbols-outlined" onClick={clickMenu}>menu</span>
+            <menu id='itens' style={{ display: itensVisible ? 'block' : 'none' }}>
+            <Link onClick={hideMenu} to='/'>Home</Link>
+            <Link onClick={hideMenu} to='/historia'>História</Link>
+            <Link onClick={hideMenu} to='/titulos'>Títulos</Link>
+            <Link onClick={hideMenu} to='/estatisticas'>Estatísticas</Link>
+            <Link onClick={hideMenu} to='/videos'>Vídeos</Link>
+            </menu>
+        </div>
     )
 }
