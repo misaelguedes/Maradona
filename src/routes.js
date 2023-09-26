af13c116ed49f089dc683aab0eea8f78ea5fd99d
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-26 12:47:32 -0300

    Criando as páginas História e Erro

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ import Header from './components/Header'
 import Menu from './components/Menu'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import História from './pages/História'
 import Títulos from './pages/Títulos'
 import Estatísticas from './pages/Estatísticas'
 import Vídeos from './pages/Vídeos'
+import Erro from './pages/Erro'
 
 export default function RoutesApp() {
     return (
@@ -15,9 +17,12 @@ export default function RoutesApp() {
             <Menu/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/historia' element={<História/>}/>
                 <Route path='/titulos' element={<Títulos/>}/>
                 <Route path='/estatisticas' element={<Estatísticas/>}/>
                 <Route path='/videos' element={<Vídeos/>}/>
+
+                <Route path='*' element={<Erro/>}/>
             </Routes>
             <Footer/>
         </BrowserRouter>
